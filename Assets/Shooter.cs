6252b6d8aa9403868016f6c11ce6294415637300
selimjb <s.bennani@wewanttoknow.com>
2020-11-15 00:57:56 +0100

    Shooter quick prototype

---
+++ Assets/Shooter.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] LineRenderer lineRenderer;
+    [SerializeField] Vector3 origin = Vector2.zero;
+    [SerializeField] float maxLength = 3f;
+
+    Vector3 entryPoint;
+    Vector3 exitPoint;
+    Vector3 position;
+    Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            OnShootInit();
+        if (Input.GetMouseButton(0))
+            OnShootPrep();
+        if (Input.GetMouseButtonUp(0))
+            OnShootRelease();
+    }
+
+    void OnShootInit()
+    {
+        entryPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void OnShootPrep()
+    {
+        exitPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lineRenderer.SetPosition(1, origin + (exitPoint - entryPoint));
+    }
+
+    void OnShootRelease()
+    {
+        lineRenderer.SetPosition(1, origin);
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Shoot");
+    }
+}
