3e6650521abc6db61984e0269ba8c1dc4174e76d
selimjb <s.bennani@wewanttoknow.com>
2020-12-31 01:05:07 +0100

    Create Title screen

---
+++ Assets/Script/TitleScreenMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScreenMenu : MonoBehaviour
+{
+    [SerializeField] Button startButton;
+
+    // FIXME : On pointer down
+    private void Start()
+    {
+        startButton.onClick.AddListener(LoadGame);
+    }
+
+    private void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
