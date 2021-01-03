898df09189979ec67ec064386149b0fde0e567ec
selimjb <s.bennani@wewanttoknow.com>
2021-01-03 02:20:57 +0100

    Create simple swarm generator

---
+++ Assets/SwarmGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SwarmGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject SwarmPrefab;
+
+    void Start()
+    {
+        StartCoroutine(GenerateSwarm());
+    }
+
+    IEnumerator GenerateSwarm()
+    {
+        var pos = new Vector3(Random.Range(0f, 1f) > 0.5f ? -22 : 22, Random.Range(-15f, 15f), 0);
+        Instantiate(SwarmPrefab, pos, Quaternion.identity);
+        yield return new WaitForSeconds(4f);
+        yield return GenerateSwarm();
+    }
+}
