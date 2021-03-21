bbc794d27bd037fe0111a34d1bc61107256bc9f2
selimjb <s.bennani@wewanttoknow.com>
2021-03-21 13:55:56 +0100

    Add git grep utility

---
+++ Assets/Script/Utilities/GitGrep.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class GitGrep
+{
+	[MenuItem("Assets/Copy Git Grep Guid", false, 100000)]
+	public static void CopyGitGrepGUID()
+	{
+		string command = "";
+		int i = 0;
+
+		foreach (var item in Selection.assetGUIDs)
+		{
+			command += "git grep " + item + (i < Selection.assetGUIDs.Length ? "\r" : "");
+			i++;
+		}
+
+		Debug.Log(command);
+		EditorGUIUtility.systemCopyBuffer = command;
+	}
+}
